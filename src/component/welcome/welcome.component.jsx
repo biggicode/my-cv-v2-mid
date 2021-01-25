0c720de4e472224e0656511362b26d36387a5f78
Lari Oncescu <larioncescu@yahoo.com>
2021-01-25 17:07:01 +0200

    Adding useState and useEffect

--- src/component/welcome/welcome.component.jsx
+++ src/component/welcome/welcome.component.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import * as WS from './welcome.style'
 
 const Welcome = () => {
+  const [test, modificaTest] = useState({})
+
+  useEffect(
+    () => {
+      fetch("http://localhost:5000/welcome").then(
+        result => result.json()
+
+      ).then(result => modificaTest(result))
+      // modificaTest("mere")
+    },
+    []
+  )
+
   return (
     <div>
       <h1>Welcome title</h1>
+      <WS.StyleMainTitle>{test.name}</WS.StyleMainTitle>
+      <p>{test.profession}</p>
     </div>
   )
 }
---
+++ src/component/welcome/welcome.style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const StyleMainTitle = styled.h1`
+  color: red;
+`
