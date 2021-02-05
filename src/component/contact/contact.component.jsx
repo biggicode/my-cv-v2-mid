b2a40ccad1a4cd1f184c6abe50c644479a0cf38c
Lari Oncescu <larioncescu@yahoo.com>
2021-02-05 14:42:18 +0200

    Adding data to the component

--- src/component/contact/contact.component.jsx
+++ src/component/contact/contact.component.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as CS from './contact.style'
 import { Header } from '../header'
 
 const Contact = () => {
+
+  const [dateContact, schimbaDate] = useState({})
+
+  useEffect(
+    () => {
+      try {
+        const getResult = async () => {
+          const rezultat = await(await fetch("http://localhost:5000/contact")).json()
+          schimbaDate(rezultat)
+        }
+        getResult()
+      } catch (error) {
+        console.log("ceva nu e bine", error)
+      }
+    },
+    []
+  )
+
+
   return (
     <CS.StyledGrid>
-      <Header />
+      <Header title={dateContact.sectionTitle}/>
     </CS.StyledGrid>
   )
 }
