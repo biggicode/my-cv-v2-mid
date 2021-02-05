0ed2316e0b804798062343d4a32396ca9eee6563
Lari Oncescu <larioncescu@yahoo.com>
2021-02-05 12:48:46 +0200

    Deleting old content from contact section

--- src/component/contact/contact.component.jsx
+++ src/component/contact/contact.component.jsx
@@ -3,14 +3,9 @@ import * as CS from './contact.style'
 
 const Contact = () => {
   return (
-    <CS.StyledContact>
-      <CS.StyledParagraf>I like eating oatmeal</CS.StyledParagraf>
-      <CS.StyledUnorderedList>
-        <CS.StyledListItem>Mere</CS.StyledListItem>
-        <CS.StyledListItem>Pere</CS.StyledListItem>
-        <CS.StyledListItem>Capsuni</CS.StyledListItem>
-      </CS.StyledUnorderedList>
-    </CS.StyledContact>
+    <div>
+      
+    </div>
   )
 }
 
