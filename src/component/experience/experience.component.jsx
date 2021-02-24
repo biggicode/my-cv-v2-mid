b0fbe972857e48f8921b009f289c3937a6b282ce
Lari Oncescu <larioncescu@yahoo.com>
2021-02-24 20:01:45 +0200

    Making new section

---
+++ src/component/experience/experience.component.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import * as ES from './experience.style'
+import { Header } from '../header'
+
+export const Experience = () => {
+
+  const [dateExperience, setDateExperience] = useState({})
+
+  useEffect(() => {
+    try {
+      const getResult = async () => {
+        const result = await(await fetch('http://localhost:5000/experience')).json()
+        setDateExperience(result)
+      }
+      getResult
+    } catch (error) {
+      console.log("Ceva nu merge", error)
+    }
+  },
+  []
+  )
+
+  return (
+    <ES.GridContainer>
+      <Header title={dateExperience.sectionTitle}/>
+    </ES.GridContainer>
+  )
+}
