fa84321d161245748b924547229eb7802d5a9df6
Lari Oncescu <larioncescu@yahoo.com>
2021-03-02 22:02:24 +0200

    Making fetch and title for new section component

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -3,6 +3,7 @@ import { Contact } from '../contact'
 import { Skills } from '../skills'
 import { Welcome } from '../welcome'
 import { Experience } from '../experience'
+import { OpenSource } from '../open-source'
 import * as AS from './app.style'
 
 const App = () => {
--- src/component/open-source/open-source.component.jsx
+++ src/component/open-source/open-source.component.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as OSS from './open-source.style'
+import { Header } from '../header'
+import { HeaderIcon } from '../header-icon'
 
 export const OpenSource = () => {
-  return (
-    <div>
 
-    </div>
+  const [dateOpenSource, setDateOpenSource] = useState({})
+
+  useEffect(
+    () => {
+      try {
+        const getResult = async () => {
+          const rezultat = await(await fetch("http://localhost:5000/open-source")).json()
+          setDateOpenSource(rezultat)
+        }
+        getResult()
+      }catch(error) {
+        console.log("ceva nu functioneste",error)
+      }
+
+    },
+    []
+  )
+
+
+  return (
+    <OSS.StyledGrid>
+      <Header title={dateOpenSource.sectionTitle}/>
+      
+      <HeaderIcon />
+
+    </OSS.StyledGrid>
   )
 }
