194436301f40861ccb94b8af1ba1806d99280686
Lari Oncescu <larioncescu@yahoo.com>
2021-03-09 14:46:51 +0200

    Adding grid and grid row

---
+++ src/component/portfolio/portfolio.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+`
+
+export const StyledRow = styled.div`
+  grid-column: 1/-1;
+`
