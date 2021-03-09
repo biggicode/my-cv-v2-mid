34e8d91aab0ee794b2eadbfc8da561d383ff89eb
Lari Oncescu <larioncescu@yahoo.com>
2021-03-09 14:44:32 +0200

    Adding fetch and useState  to display data

--- src/component/portfolio/portfolio.component.jsx
+++ src/component/portfolio/portfolio.component.jsx
@@ -2,9 +2,26 @@ import React, { useEffect, useState} from 'react'
 import * as PS from './portfolio.style'
 
 export const Portfolio = () => {
+
+  const [datePortfolio, setDatePortfolio] = useState({})
+
+  useEffect(() => {
+    try {
+      const getResult = async () => {
+        const result = await(await fetch('http://localhost:5000/portfolio')).json()
+        setDatePortfolio(result)
+      }
+      getResult()
+    } catch(error) {
+      console.log("Something wrong",error)
+    }
+  },
+  []
+  )
+
   return(
     <div>
-      
+      {datePortfolio.sectionTitle}
     </div>
   )
 }
