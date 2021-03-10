66aecb0f1c87c61023c933cf1826c90526ac7d63
Lari Oncescu <larioncescu@yahoo.com>
2021-03-10 14:41:18 +0200

    Making useState and useEffect for section

--- src/component/recommendations/recommendations.component.jsx
+++ src/component/recommendations/recommendations.component.jsx
@@ -3,6 +3,22 @@ import * as RS from './recommendations.style'
 
 export const Recommendations = () => {
 
+  const [dateRecom, setDateRecom] = useState({})
+
+  useEffect(() => {
+    try {
+      const getResult = async () => {
+        const result = await(await fetch('http://localhost:5000/recommendations')).json()
+        setDateRecom(result)
+      }
+      getResult()
+    } catch(error) {
+      console.log("Something it's wrong",error)
+    }
+  },
+  []
+  )
+
   return (
     <div>
       
