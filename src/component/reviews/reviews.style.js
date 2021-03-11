e0ca3b2ff1bf147c9a69d2014f9eea3ae6af8c19
Lari Oncescu <larioncescu@yahoo.com>
2021-03-11 15:07:22 +0200

    Styling the content box

--- src/component/reviews/reviews.style.js
+++ src/component/reviews/reviews.style.js
@@ -5,3 +5,32 @@ export const StyledGrid = styled.div`
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 10px;
 `
+
+export const StyledReview = styled.div`
+  grid-column: 1/-1;
+`
+
+export const StyledReviewHeader = styled.div`
+  display: flex;
+`
+
+export const StyledReviewContentBox = styled.div`
+  border-radius: 6px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: #fff;
+  padding: 20px;
+  position: relative;
+
+  &::after {
+    content: '';
+    width: 15px;
+    height: 15px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.25);
+    background-color: #fff;
+    position: absolute;
+    top: -7px;
+    left: 15px;
+    transform: rotate(45deg);
+    clip-path: polygon(-6px -6px, 100% 0, 0 100%);
+  }
+`
