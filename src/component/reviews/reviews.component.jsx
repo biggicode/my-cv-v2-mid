9f5a8292b914f4ee2825bae56b0c14535293e509
Lari Oncescu <larioncescu@yahoo.com>
2021-03-11 14:40:06 +0200

    Fetch data with useFffect and useState

--- src/component/reviews/reviews.component.jsx
+++ src/component/reviews/reviews.component.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import * as RES from './reviews.style'
 
 export const Reviews = () => {
+
+  const [dateReviews, setDateReviews] = useState({})
+
+  useEffect(() => {
+    try{
+      const getResult = async () => {
+        const result = await(await fetch('http://localhost:5000/reviews')).json()
+        setDateReviews(result)
+      }
+      getResult()
+    } catch(error) {
+      console.log("Something at reviews it's wrong",error)
+    }
+  },
+  []
+  )
+
   return (
     <div>
       
