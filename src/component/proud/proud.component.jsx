be20bf4307aa04288f6854aeb810f767a3aac300
Lari Oncescu <larioncescu@yahoo.com>
2021-03-12 21:12:20 +0200

    Styling grid and importing it

--- src/component/proud/proud.component.jsx
+++ src/component/proud/proud.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import * as SP from './proud.style'
 
 export const Proud = () => {
 
@@ -19,8 +20,8 @@ export const Proud = () => {
   )
 
   return (
-    <div>
+    <SP.StyledGrid>
       
-    </div>
+    </SP.StyledGrid>
   )
 }
