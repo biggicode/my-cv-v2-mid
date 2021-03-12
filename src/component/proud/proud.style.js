5afc1fad74246f6c1e31bfbac88cde550a262bfd
Lari Oncescu <larioncescu@yahoo.com>
2021-03-12 21:33:05 +0200

    Making the last styles

--- src/component/proud/proud.style.js
+++ src/component/proud/proud.style.js
@@ -11,10 +11,16 @@ export const StyledUl = styled.ul`
   border: 2px solid #707070;
   border-radius: 6px;
   grid-column: span 4;
+  list-style-type: none;
 `
 
 export const StyledLi = styled.li`
   padding: 10px 22px;
+  border-bottom: 2px solid #707070;
+
+  &:last-child {
+    border-bottom: none;
+  }
 `
 
 //Text 
@@ -25,7 +31,7 @@ export const StyledDate = styled.p`
   font-weight: bold;
   color: #707070;
   margin-bottom: 10px;
-  text-align: left;
+  text-align: right;
 `
 
 export const StyledText = styled.p`
