86fc93f718c06d2d418eefab317e231781e6ce55
Lari Oncescu <larioncescu@yahoo.com>
2021-03-12 21:07:28 +0200

    Adding useState and useEffect to the comp

--- src/component/proud/proud.component.jsx
+++ src/component/proud/proud.component.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Proud = () => {
+
+  const [dateProud,setDateProud] = useState({})
+
+  useEffect(() => {
+    try {
+      const getResult = async () => {
+        const result = await (await fetch('http://localhost:5000/proud')).json()
+        setDateProud(result)
+      }
+      getResult()
+    } catch(error) {
+      console.log("Something it's wrong with proud section!",error)
+    }
+  },
+  []
+  )
 
   return (
     <div>
