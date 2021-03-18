6f5dfbc337cbfa101e9fed606ee040d9dd15cae4
Lari Oncescu <larioncescu@yahoo.com>
2021-03-18 10:57:03 +0200

    Fixing menu functionality

--- src/component/menu/menu.component.jsx
+++ src/component/menu/menu.component.jsx
@@ -1,44 +1,49 @@
-import React, {useState, useEffect} from 'react'
-import * as MS from './menu.style'
+import React, { useState, useEffect } from "react";
+import * as MS from "./menu.style";
 
 export const Menu = () => {
-
-  const [dateMenu,setDateMenu] = useState({})
+  const [dateMenu, setDateMenu] = useState({});
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     try {
       const getResult = async () => {
-        const result = await(await fetch('http://localhost:5000/menu')).json()
-        setDateMenu(result)
-      }
-      getResult()
-    } catch(error) {
-      console.log("Something it's wrong with menu",error)
+        const result = await (await fetch("http://localhost:5000/menu")).json();
+        setDateMenu(result);
+      };
+      getResult();
+    } catch (error) {
+      console.log("Something it's wrong with menu", error);
     }
-  },[])
-
-  let show = true;
+  }, []);
 
   const handleClick = () => {
     setDateMenu({
       ...dateMenu,
-      show: !show
-    })
-  }
+    });
+    setShowMenu(!showMenu);
+  };
 
   return (
-    <MS.Menu onClick={() => handleClick()}>
-      { show && 
-      <MS.Hamburger>
-        <MS.HamburgerLineOne />
-        <MS.HamburgerLineTwo />
-        <MS.HamburgerLineThree />
-      </MS.Hamburger>}
-      
-      { !show && {dateMenu.menuList?.map((link)=>(
-        <MS.StyledMenuLinks>{link}</MS.StyledMenuLinks>
-      ))}
-      <MS.StyledMenuLinks><span className="icon__cross"></span></MS.StyledMenuLinks>}
-    </MS.Menu>
-  )
-}
+    <>
+      {showMenu ? (
+        <MS.Menu2 onClick={handleClick}>
+          <MS.Hamburger>
+            <MS.HamburgerLineOne />
+            <MS.HamburgerLineTwo />
+            <MS.HamburgerLineThree />
+          </MS.Hamburger>
+        </MS.Menu2>
+      ) : (
+        <MS.Menu onClick={handleClick}>
+          {dateMenu.menuList?.map((link) => (
+            <MS.StyledMenuLinks>{link}</MS.StyledMenuLinks>
+          ))}
+          <MS.StyledMenuLinks>
+            <span className="icon__cross"></span>
+          </MS.StyledMenuLinks>
+        </MS.Menu>
+      )}
+    </>
+  );
+};
