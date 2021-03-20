63d2ae2eae41dfa0d8bcaccacdd72f4d62cdb6b4
Lari Oncescu <larioncescu@yahoo.com>
2021-03-20 14:17:09 +0200

    Making skills component responsive for desktop

--- src/component/skills/skills.style.js
+++ src/component/skills/skills.style.js
@@ -10,10 +10,14 @@ export const StyledGrid = styled.div`
     grid-template-columns: repeat(8, 1fr);
     grid-gap: 16px;
   }
+
+  @media (min-width: 1366px) {
+    grid-template-columns: repeat(12, 1fr);
+  }
 `
 
 export const StyledRow = styled.div`
-  grid-column: 1/-1;
+  grid-column: span 9;
 `
 
 export const StyledUl = styled.ul`
