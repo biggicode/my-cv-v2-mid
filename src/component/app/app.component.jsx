45259f63faf561df1933249486c8fcc59e352464
Lari Oncescu <larioncescu@yahoo.com>
2021-03-24 17:03:57 +0200

    Rezolving problem with overlay

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -25,7 +25,6 @@ const App = () => {
       <Reviews />
       <Proud />
       <Menu />
-      <Overlay />
     </AS.StyledApp>
   )
 }
--- src/component/overlay/overlay.component.jsx
+++ src/component/overlay/overlay.component.jsx
@@ -1,26 +1,16 @@
-import React, { useState }from 'react'
-import * as OS from './overlay.style'
-import { crossConfig } from '../icomoon/config'
+import React, { useState } from "react";
+import * as OS from "./overlay.style";
+import { crossConfig } from "../icomoon/config";
 
-export const Overlay = ({handleClick}) => {
-
-  const [showOverlay, setShowOverlay] = useState(true);
-
-  const handleClick = () => {
-    setShowOverlay(!showOverlay);
-  }
-
+export const Overlay = ({close}) => {
   return (
     <>
-      {
-        showOverlay &&  <OS.Overlay>
+      <OS.Overlay>
         <OS.StyledPhone src="asset/image/phone.svg" />
         <OS.StyledRotate src="asset/image/rotate.svg" />
-        <OS.StyledCloseIcon {...crossConfig}  onClick={handleClick}/>
+        <OS.StyledCloseIcon {...crossConfig} onClick={close} />
         <OS.StyledIframe src="https://www.youtube.com/embed/nX2M7nzXWqA"></OS.StyledIframe>
-        </OS.Overlay>
-      }
-      
+      </OS.Overlay>
     </>
-  )
-}
+  );
+};
